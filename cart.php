7ddaf09e76b0fd28686b9976faab8d4284d9aeb6
Srishty Sharma <84490597+srishty1101@users.noreply.github.com>
2021-10-08 23:08:32 +0530

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Cart | E Store.com</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+  <body style="padding-top: 50px;">
+       <!-- Header -->
+       <?php
+       include 'includes/header.php';
+       ?>
+       <!--Header end-->
+       
+       <div class="container">
+              <div class="row">
+               <div class="col-sm-6 col-sm-offset-3">
+                   <table class="table table-striped">
+
+                       <!--show table only if there are items added in the cart-->
+                       <?php
+                       $sum = 0;
+                       $user_id = $_SESSION['user_id'];
+                       $query = "SELECT items.price AS Price, items.id, items.name AS Name FROM users_items JOIN items ON users_items.item_id = items.id WHERE users_items.user_id='$user_id' and status='Added to cart'";
+                       $result = mysqli_query($con, $query)or die($mysqli_error($con));
+                       if (mysqli_num_rows($result) >= 1) {
+                           ?>
+                           <thead>
+                            <tr>
+                                <th>Item Number</th>
+                                <th>Item Name</th>
+                                <th>Price</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        
+                        <tbody>
+                            <?php
+                                while ($row = mysqli_fetch_array($result)) {
+                                    $sum+= $row["Price"];
+                                    $id="";
+                                    $id .= $row["id"] . ",";
+                                    echo "<tr>
+                                              <td>" . "#" . $row["id"] . "</td>
+                                              <td>" . $row["Name"] . "</td>
+                                              <td>Rs " . $row["Price"] . "</td>
+                                              <td><a href='cart-remove.php?id={$row['id']}' class='remove_item_link'> X </a></td>
+                                          </tr>";
+                                }
+                                $id = rtrim($id, ",");
+                                echo "<tr>
+                                          <td></td>
+                                          <td>Total</td>
+                                          <td>Rs " . $sum . "</td>
+                                          <td><a href='success.php?itemsid=".$id ."'class='btn btn-primary'>Confirm Order</a></td>
+                                          </tr>";
+                                ?>
+                            </tbody>
+                            <?php
+                        } else {
+                            echo "<center><h2>Add items to the cart first!</h2></center>";
+                        }
+                        ?>
+                        <?php
+                        ?>
+                    </table>
+                </div>
+             </div>
+        </div>
+        
+        <!--Footer-->
+        <?php include 'includes/footer.php';?>
+        
+        <!--Footer end-->
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
