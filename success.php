e58a96f6876899f0c4cbb6b2daea2baafddf3ac8
Srishty Sharma <84490597+srishty1101@users.noreply.github.com>
2021-10-08 23:03:39 +0530

    Create success.php

---
+++ success.php
@@ -0,0 +1,46 @@
+<?php
+
+require("includes/common.php");
+if (!isset($_SESSION['email'])) {
+    header('location: index.php');
+}
+$user_id = $_SESSION['user_id'];
+$item_ids_string = $_GET['itemsid'];
+
+//We will change the status of the items purchased by the user to 'Confirmed'
+$query = "UPDATE users_items SET status='Confirmed' WHERE user_id=" . $user_id . " AND item_id IN (" . $item_ids_string . ") and status='Added to cart'";
+mysqli_query($con, $query) or die($mysqli_error($con));
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Success | Life Style Store</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+  <body>
+    <?php include 'includes/header.php'; ?>
+    <div class="container-fluid" id="content">
+        <div class="col-md-12">
+            <div class="jumbotron">
+
+                  <h3 align="center">Your order is confirmed. Thank you for shopping with us.</h3><hr>
+                <p align="center">Click <a href="products.php">here</a> to purchase any other item.</p>
+            </div>
+        </div>
+    </div>
+    <?php include("includes/footer.php");
+    ?>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
